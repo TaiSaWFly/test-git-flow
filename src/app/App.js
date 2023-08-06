fea078c8495a59e665a4b22f6831d1f75f2ceca6
TaiSaWFly <vawer24@gmail.com>
2023-08-06 19:49:45 +0300

    env2

--- src/app/App.js
+++ src/app/App.js
@@ -1,6 +1,16 @@
 function App() {
   console.log(process.env);
 
+  if (process.env.NODE_ENV === "development") {
+    console.log(process.env.REACT_APP_SKDEV);
+    console.log(process.env.REACT_APP_SKPROD);
+  }
+
+  if (process.env.NODE_ENV === "production") {
+    console.log(process.env.REACT_APP_SKDEV);
+    console.log(process.env.REACT_APP_SKPROD);
+  }
+
   return <div>Hello</div>;
 }
 
