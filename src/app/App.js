3861c7d1ef3f6900608dd1051b540adb0d1a9a29
TaiSaWFly <vawer24@gmail.com>
2023-08-07 01:37:28 +0300

    test routing gh-pages

--- src/app/App.js
+++ src/app/App.js
@@ -1,4 +1,19 @@
-function App() {
+import { Link } from "react-router-dom/cjs/react-router-dom";
+import {
+  Redirect,
+  Route,
+  Switch,
+} from "react-router-dom/cjs/react-router-dom.min";
+
+const Home = () => {
+  return <div>Home component</div>;
+};
+
+const Aboute = () => {
+  return <div>Aboute component</div>;
+};
+
+const App = () => {
   console.log(process.env);
 
   if (process.env.NODE_ENV === "development") {
@@ -11,7 +26,26 @@ function App() {
     console.log(process.env.REACT_APP_SKPROD);
   }
 
-  return <div>Hello is run to GH actions git branch production edddit</div>;
-}
+  return (
+    <div>
+      <div
+        style={{
+          marginBottom: "20px",
+        }}
+      >
+        <Link to="/">Home</Link>
+        <Link to="/aboute">Aboute</Link>
+      </div>
+      <div> Hello is run to GH actions git branch production edddit</div>
+
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/aboute" component={Aboute} />
+
+        <Redirect to="/" from="*" />
+      </Switch>
+    </div>
+  );
+};
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -2,10 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./app/App";
+import { Router } from "react-router-dom";
+import { createBrowserHistory } from "history";
+
+const history = createBrowserHistory();
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   // <React.StrictMode>
-  <App />
+  <Router history={history}>
+    <App />
+  </Router>
+
   // </React.StrictMode>
 );
